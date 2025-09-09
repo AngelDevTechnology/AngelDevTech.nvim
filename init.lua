3c7d04d72dea10980565d559f333f69ef020a924
AngelDevTechnology <lapeyre.angel5@gmail.com>
2025-09-09 22:34:32 +0200

    Init.lua file main

---
+++ init.lua
@@ -0,0 +1,27 @@
+-- ==============================
+-- Lazy.nvim bootstrap
+-- ==============================
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ==============================
+-- Plugins
+-- ==============================
+local plugins = require("plugins")
+local opts = {}
+
+require("keymaps")
+require("options")
+require("lazy").setup("plugins")
+
+	
